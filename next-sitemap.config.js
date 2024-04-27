c9504ca5ee30fd85ea59f0623f5f04b8fe7fb304
Tatsumi0000 <19218690+Tatsumi0000@users.noreply.github.com>
2024-04-28 03:51:31 +0900

    site-mapに登録しないページを追加

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -5,6 +5,7 @@ module.exports = {
   generateRobotsTxt: true,
   sitemapSize: 7000,
   generateIndexSitemap: false,
+  exclude: ['/tag/*', '/about', '/-/*'],
   // ...other options
   // https://github.com/iamvishnusankar/next-sitemap#configuration-options
 }
